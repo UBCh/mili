1d85315a2787d51ce33db24ccd26b33340ea4443
UBCh <u2b127@gmail.ru>
2021-10-03 13:03:04 +0300

    result

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 8500;
+        int bonus = 20;
+        int mili = ticket / bonus;
+        System.out.println(mili);
+    }
+}
